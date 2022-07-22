3b932af96a64cc83549989657156493d023d0a27
marcos-pdvni <102670358+marcos-pdvni@users.noreply.github.com>
2022-07-22 14:39:36 -0300

    Add files via upload

--- controller/register.js
+++ controller/register.js
@@ -4,23 +4,27 @@ const bcrypt = require("bcryptjs");
 exports.createUser = async (req, res) => {
   const { username, password, secret, secretAnswer } = req.body;
 
-  const user = await User.findOne({
-    username: username,
-  });
+  try {
+    const user = await User.findOne({
+      username: username,
+    });
 
-  if (user) return res.status(406).json({ message: "User already exists." });
+    if (user) return res.status(406).json({ message: "User already exists." });
 
-  const encryptedPassword = await bcrypt.hash(password, 12);
-  const encryptedAnswer = await bcrypt.hash(secretAnswer, 12);
+    const encryptedPassword = await bcrypt.hash(password, 12);
+    const encryptedAnswer = await bcrypt.hash(secretAnswer, 12);
 
-  const newUser = await User.create({
-    username: username,
-    password: encryptedPassword,
-    secretQuestion: secret,
-    secretAnswer: encryptedAnswer,
-  });
+    const newUser = await User.create({
+      username: username,
+      password: encryptedPassword,
+      secretQuestion: secret,
+      secretAnswer: encryptedAnswer,
+    });
 
-  await newUser.save();
+    await newUser.save();
 
-  res.status(201).json({ message: "User succesfully created." });
+    res.status(201).json({ message: "User succesfully created." });
+  } catch (error) {
+    console.log(error);
+  }
 };

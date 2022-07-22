d0f432d335a3a096dd4ecf067583aa895652850e
marcos-pdvni <102670358+marcos-pdvni@users.noreply.github.com>
2022-07-22 16:58:11 -0300

    Add files via upload

--- controller/auth.js
+++ controller/auth.js
@@ -1,14 +1,37 @@
+const User = require("../model/user");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.authUser = async (req, res) => {
-  const { username } = req.body;
+  const { username, password } = req.body;
 
-  const token = jwt.sign(
-    {
+  try {
+    const user = await User.findOne({
       username: username,
-    },
-    process.env.JWT_SECRET
-  );
+    }).select("+password");
 
-  return res.status(200).cookie("uauth", token).redirect("/application");
+    if (!user) return res.status(404).json({ message: "User not found!" });
+
+    if (!(await bcrypt.compare(password, user.password)))
+      return res
+        .status(401)
+        .json({ message: "Username or password incorrect!" });
+
+    const token = jwt.sign(
+      {
+        username: user.username,
+        secret: user.secretQuestion,
+        imageUrl: user.profileImage,
+        userFriends: user.friends,
+      },
+      process.env.JWT_SECRET
+    );
+
+    return res
+      .status(200)
+      .cookie("uauth", token)
+      .json({ message: "Logged in." });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.set("view engine", "ejs");
 
 // routes
 const login = require("./routes/login");
-
+const register = require("./routes/register");
 const users = require("./routes/users");
-
+const reset = require("./routes/reset");
 const auth = require("./routes/auth");
 const application = require("./routes/application");
 const logout = require("./routes/logout");
@@ -31,7 +31,9 @@ const logout = require("./routes/logout");
 app.use(logout);
 app.use(application);
 app.use(auth);
+app.use(reset);
 app.use(users);
+app.use(register);
 app.use(login);
 
 app.use((req, res) => {
@@ -76,12 +78,19 @@ io.on("connection", async (socket) => {
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect(
-  `mongodb+srv://marcos:${process.env.DB_PASS}@cluster0.fe4kkjn.mongodb.net/?retryWrites=true&w=majority`,
-  () => {
-    console.log("Mongo connected.");
-    server.listen(port, () => {
-      console.log("Server running on port", port);
-    });
+async function connectDb() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://marcos:${process.env.DB_PASS}@cluster0.fe4kkjn.mongodb.net/chat?retryWrites=true&w=majority`,
+      () => {
+        console.log("Mongo connected.");
+        server.listen(port, () => {
+          console.log("Server runnin on port", port);
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
   }
-);
+}
+connectDb();
